c796dcf48438766c7db940011b023a7f2fa93875
DH <adlandh@gmail.com>
2023-11-12 16:39:00 +0100

    response dumper as separate package

--- middleware_test.go
+++ middleware_test.go
@@ -42,13 +42,61 @@ func (s *MiddlewareTestSuite) SetupTest() {
 	})
 	s.NoError(err)
 	s.e = echo.New()
+}
+
+func (s *MiddlewareTestSuite) TestMiddleware() {
+	s.e.Use(Middleware())
+
+	s.Run("Test Get", func() {
+		s.e.GET("/", func(c echo.Context) error {
+			span := sentry.TransactionFromContext(c.Request().Context())
+			s.NotNil(span)
+			s.NotEmpty(span.SpanID)
+			s.NotEmpty(span.Tags["client_ip"])
+			s.Equal(echo.MIMEApplicationJSON, span.Tags["req.header.Content-Type"])
+			s.Equal("test", span.Tags["req.header.Testheader"])
+			return c.String(http.StatusOK, "test")
+		})
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set("testHeader", "test")
+		rec := httptest.NewRecorder()
+		s.e.ServeHTTP(rec, req)
+		s.Equal(http.StatusOK, rec.Code)
+		body, err := io.ReadAll(rec.Body)
+		s.NoError(err)
+		s.Equal("test", string(body))
+	})
+	s.Run("Test Post", func() {
+		s.e.POST("/", func(c echo.Context) error {
+			span := sentry.TransactionFromContext(c.Request().Context())
+			s.NotNil(span)
+			s.NotEmpty(span.SpanID)
+			s.NotEmpty(span.Tags["client_ip"])
+			s.Equal(echo.MIMEApplicationJSON, span.Tags["req.header.Content-Type"])
+			s.Equal("test", span.Tags["req.header.Testheader"])
+			s.Empty(span.Tags["req.body"])
+			return c.String(http.StatusOK, "test")
+		})
+	})
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("testBody"))
+	req.Header.Set(echo.HeaderContentType, echo.MIMETextPlain)
+	req.Header.Set("testHeader", "test")
+	rec := httptest.NewRecorder()
+	s.e.ServeHTTP(rec, req)
+	s.Equal(http.StatusOK, rec.Code)
+	body, err := io.ReadAll(rec.Body)
+	s.NoError(err)
+	s.Equal("test", string(body))
+}
+
+func (s *MiddlewareTestSuite) TestMiddlewareWithConfig() {
 	s.e.Use(MiddlewareWithConfig(SentryConfig{
 		AreHeadersDump: true,
 		IsBodyDump:     true,
 	}))
-}
 
-func (s *MiddlewareTestSuite) TestMiddlewareWithConfig() {
 	s.Run("Test Get", func() {
 		s.e.GET("/", func(c echo.Context) error {
 			span := sentry.TransactionFromContext(c.Request().Context())
